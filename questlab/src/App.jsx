80089f05534ae14ee5ce60c25cfc4ca8bee14be6
Ganesh Kumar <mrganeshofficial8@gmail.com>
2024-06-21 18:10:40 +0530

    button added

--- questlab/src/App.jsx
+++ questlab/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { FormData } from "./Components/Form";
+import { Home } from "./Components/Home";
 import { Navbar } from "./Components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Home/>
       <FormData />
     </>
   );
---
+++ questlab/src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css"
+
+export const Home = () =>{
+
+    return <>
+    <div className="home_page">
+        <p>Welcome to QuestLab</p>
+        <button className="home_btn">Connect with Sales</button>
+    </div>
+    
+    </>
+}
