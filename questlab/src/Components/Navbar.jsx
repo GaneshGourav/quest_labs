419909ae9fe2e8c1efe5f4cfb077225b7cd66a0d
Ganesh Kumar <mrganeshofficial8@gmail.com>
2024-06-21 17:59:36 +0530

    navbar created

---
+++ questlab/src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+export const Navbar = () =>{
+
+    return <>
+    <nav className="nav">
+        <div >
+            <p>QuestLabs</p>
+        </div>
+        <div className="nav-title">
+            <a href="#">Product</a>
+            <a href="#">Solution</a>
+            <a href="#">Customers</a>
+            <a href="#">Signup</a>
+            <a href="#">Login</a>
+        </div>
+     
+    </nav>
+    <hr />
+    
+    
+    </>
+}
